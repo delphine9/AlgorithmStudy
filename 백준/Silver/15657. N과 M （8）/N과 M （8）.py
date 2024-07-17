bbc5ce0ac1907d4ade4aa81b63cb4c11b590cc55
delphine9 <psj2145@naver.com>
2024-07-17 11:22:54 +0900

    [Silver III] Title: N과 M (8), Time: 44 ms, Memory: 31364 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,26 @@
+def pprint():
+    # arrayList를 문자열로 받아 result 배열에 공백을 두고 넣기
+    result.append(' '.join(map(str, arrayList)))
+
+
+def dfs(v, h):
+    if h == m:  # m이라면
+        pprint()  # pprint함수
+        return
+
+    for i in range(v, n+1):  # v부터 n까지 반복
+        arrayList.append(array2[i-1])  # 1부터 시작했기에 i-1 넣고
+        dfs(i, h+1)  # 1씩 추가해주다가
+        arrayList.pop()  # m이 되면 pop
+
+
+n, m = map(int, input().split())
+array = list(map(int, input().split()))  # list로 받기
+array2 = sorted(array)  # 받은 리스트 오름차순 정렬
+arrayList = []  # 넣었다 빼고 반복할 배열
+result = []  # 최종 결과물을 담은 배열
+visited = [False] * n  # visited 초기화
+
+
+dfs(1, 0)
+print('\n'.join(result))  # 결과 한번에 출력
