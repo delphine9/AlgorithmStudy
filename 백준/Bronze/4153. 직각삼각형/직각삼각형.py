dba7dd8c64d04b235a1ba4342c133e21744835f2
delphine9 <psj2145@naver.com>
2024-10-29 15:28:20 +0900

    [Bronze III] Title: 직각삼각형, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.py
@@ -0,0 +1,21 @@
+import sys
+
+input = sys.stdin.readline
+
+result = []
+
+while True:
+    a, b, c = map(int, input().split())
+    if a == 0 and b == 0 and c == 0:
+        break
+    if a**2 == (b**2+c**2):
+        result.append("right")
+    elif b**2 == (a**2+c**2):
+        result.append("right")
+    elif c**2 == (a**2+b**2):
+        result.append("right")
+    else:
+        result.append("wrong")
+
+for i in result:
+    print(i)
