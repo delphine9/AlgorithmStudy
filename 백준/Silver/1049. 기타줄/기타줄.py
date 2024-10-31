691431a6861c636d93fe75c395f189dbe9ab25df
delphine9 <psj2145@naver.com>
2024-10-31 15:52:36 +0900

    [Silver IV] Title: 기타줄, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1049. 기타줄/기타줄.py
+++ 백준/Silver/1049. 기타줄/기타줄.py
@@ -10,6 +10,6 @@ for pack, sing in brand:
     package = min(pack, package)
     single = min(sing, single)
 
-price = min((n//6 + (1 if n % 6 else 0)) * package,
-            (n//6) * package + (n % 6)*single, n * single)
+price = min((n//6 + 1) * package,
+            (n//6) * package + (n % 6) * single, n * single)
 print(price)
