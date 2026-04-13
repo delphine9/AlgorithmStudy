5e297528a5446e96a0137ea45770809ac63a0624
delphine9 <psj2145@naver.com>
2026-04-13 21:15:00 +0900

    [Gold I] Title: 최솟값 찾기, Time: 6208 ms, Memory: 758696 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.py
@@ -9,12 +9,12 @@ arr = list(map(int, input().split()))
 dq = deque()
 
 for i in range(n):
+    if dq and dq[0][1] < i-l+1: # 이전 인덱스면(범위 외) 제거
+        dq.popleft()
+
     while dq and dq[-1][0] > arr[i]: # 현재 값보다 큰 값 제거
         dq.pop()
 
-    dq.append((arr[i], i)) # 현재 값과 인덱스 저장
-    
-    if dq[0][1] < i-l+1: # 이전 인덱스면(범위 외) 제거
-        dq.popleft()
+    dq.append((arr[i], i)) # 현재 값과 인덱스 저장 (값, 인덱스)
 
     print(str(dq[0][0]) + " ")
